ee4dbaabc9a404a4d68546183f15b9fd1ae7b7dd
HectorMartin4 <hectormartin4.c@gmail.com>
2022-11-30 16:10:12 +0100

    Retrofit implemented

---
+++ app/src/main/java/com/example/rss_aggregator/app/data/remote/ApiClient.kt
@@ -0,0 +1,24 @@
+package com.example.rss_aggregator.app.data.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+
+class ApiClient {
+
+    private val apiServices: ApiServices
+
+    init {
+        apiServices = getApiClient()
+    }
+
+    private fun getApiClient() =
+        createRetrofitClient().create(ApiServices::class.java)
+
+    private fun createRetrofitClient() = Retrofit.Builder()
+        .baseUrl("https://google.es/") //No hace falta, se pasa dinámicamente
+        .addConverterFactory(SimpleXmlConverterFactory.create())
+        .build();
+
+    suspend fun getRss(urlRss: String) = apiServices.getRss(urlRss).body()!!
+
+}
